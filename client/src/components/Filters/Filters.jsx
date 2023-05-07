9bc3feec05b2799d1e8621baed328af24950d4cc
Lemondakii <quangductran3009@gmail.com>
2023-05-07 09:34:49 +0700

    edit css

--- client/src/components/Filters/Filters.jsx
+++ client/src/components/Filters/Filters.jsx
@@ -17,89 +17,91 @@ const Filters = () => {
   const uniqueColor = ["all", ...new Set(all_products.flatMap((p) => p.colors))];
   return (
     <div className={cx("wrapper")}>
-      <input type="text" name="text" className={cx("search-input")} placeholder="Search" onChange={updateFilter} />
-      <div className={cx("form-control")}>
-        <h5>Category</h5>
-        <div className={cx("form-category")}>
-          {uniqueCategory.map((c, index) => {
-            return (
-              <button
-                name="category"
-                key={index}
-                className={cx("btn-category", `${c === category && "active"}`)}
-                onClick={updateFilter}
-              >
-                {c}
-              </button>
-            );
-          })}
+      <div className={cx("content")}>
+        <input type="text" name="text" className={cx("search-input")} placeholder="Search" onChange={updateFilter} />
+        <div className={cx("form-control")}>
+          <h5>Category</h5>
+          <div className={cx("form-category")}>
+            {uniqueCategory.map((c, index) => {
+              return (
+                <button
+                  name="category"
+                  key={index}
+                  className={cx("btn-category", `${c === category && "active"}`)}
+                  onClick={updateFilter}
+                >
+                  {c}
+                </button>
+              );
+            })}
+          </div>
         </div>
-      </div>
-      <div className={cx("form-control")}>
-        <h5>Company</h5>
-        <select name="company" className={cx("company")} onChange={updateFilter}>
-          {uniqueCompany.map((c, index) => {
-            return (
-              <option key={index} value={c}>
-                {c}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-      <div className={cx("form-control")}>
-        <h5>Colors</h5>
-        <div className={cx("colors")}>
-          {uniqueColor.map((c, index) => {
-            if (c === "all") {
+        <div className={cx("form-control")}>
+          <h5>Company</h5>
+          <select name="company" className={cx("company")} onChange={updateFilter}>
+            {uniqueCompany.map((c, index) => {
+              return (
+                <option key={index} value={c}>
+                  {c}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div className={cx("form-control")}>
+          <h5>Colors</h5>
+          <div className={cx("colors")}>
+            {uniqueColor.map((c, index) => {
+              if (c === "all") {
+                return (
+                  <button
+                    key={index}
+                    name="colors"
+                    data-color="all"
+                    className={cx("all-btn", `${c === colors ? "active" : ""}`)}
+                    onClick={updateFilter}
+                  >
+                    All
+                  </button>
+                );
+              }
               return (
                 <button
                   key={index}
                   name="colors"
-                  data-color="all"
-                  className={cx("all-btn", `${c === colors ? "active" : ""}`)}
+                  className={cx("color-btn")}
+                  data-color={c}
                   onClick={updateFilter}
+                  style={{ backgroundColor: c }}
                 >
-                  All
+                  {colors === c ? <FaCheck /> : ""}
                 </button>
               );
-            }
-            return (
-              <button
-                key={index}
-                name="colors"
-                className={cx("color-btn")}
-                data-color={c}
-                onClick={updateFilter}
-                style={{ backgroundColor: c }}
-              >
-                {colors === c ? <FaCheck /> : ""}
-              </button>
-            );
-          })}
+            })}
+          </div>
         </div>
-      </div>
-      <div className={cx("form-control")}>
-        <h5>Price</h5>
-        <p className={cx("price")}>{formatPrice(price)}</p>
-        <input
-          name="price"
-          value={price}
-          max={max_price}
-          type="range"
-          className="price__range"
-          onChange={updateFilter}
-        />
-      </div>
-      <div className={cx("form-control")}>
-        <div className={cx("shipping")}>
-          <label htmlFor="">Free Shipping</label>
-          <input type="checkbox" name="shipping" className={cx("btn-check")} onChange={updateFilter} />
+        <div className={cx("form-control")}>
+          <h5>Price</h5>
+          <p className={cx("price")}>{formatPrice(price)}</p>
+          <input
+            name="price"
+            value={price}
+            max={max_price}
+            type="range"
+            className="price__range"
+            onChange={updateFilter}
+          />
         </div>
+        <div className={cx("form-control")}>
+          <div className={cx("shipping")}>
+            <label htmlFor="">Free Shipping</label>
+            <input type="checkbox" name="shipping" className={cx("btn-check")} onChange={updateFilter} />
+          </div>
+        </div>
+        <button type="button" className={cx("btn", "clear-btn")} onClick={clear_filter}>
+          clear filters
+        </button>
       </div>
-      <button type="button" className={cx("btn", "clear-btn")} onClick={clear_filter}>
-        clear filters
-      </button>
     </div>
   );
 };
